c81bae11aa7c10528b8f05136341ca086083536b
Chris Kunkel <ckunkel@hobokencs.net>
2021-09-22 14:12:17 -0400

    Import old code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Red_Park.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+
+@Autonomous (name = "Red_Park_Off_Sky", group = "12806")
+
+public class Red_Park extends LinearOpMode {
+
+    Robokenbot robot   = new Robokenbot();
+
+    public void runOpMode() throws InterruptedException {
+        /*
+         * Initialize the drive system variables.
+         * The init() method of the hardware class does all the work here
+         */
+        robot.init(hardwareMap,this);
+
+        waitForStart();
+
+        robot.claw.setPosition((1.0));
+
+        robot.encoderDrive(robot.DRIVE_SPEED,  -25,-25,50.0, this);  // S1: Forward 24 Inches with 5 Sec timeout
+
+        robot.strafeLeftByTime(0.4, 3000);
+
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
+    }
+
+}
