1c4a5dcea0cf5cc148ab4fdac98d3d16c9879c4b
Chris Kunkel <ckunkel@hobokencs.net>
2021-10-29 17:04:21 -0400

    AUtoPart2 Update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoPart2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoPart2.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-@Autonomous(name = "AutoPart2", group = "12806")
+@Autonomous(name = "AutoPart8", group = "12806")
 public class AutoPart2 extends LinearOpMode {
 
     Robokenbot robot = new Robokenbot();
@@ -15,18 +15,21 @@ public class AutoPart2 extends LinearOpMode {
 
         waitForStart();
         robot.claw.setPosition(1.0);
-        robot.moveArmByTime(0.5, 1500);
-        robot.strafeLeftByTime(robot.DRIVE_SPEED, 1000);
+        sleep(500);
+        robot.moveArmByTime(0.75, 2000);
+        robot.strafeLeftByTime(robot.DRIVE_SPEED * 2, 1600);
+        robot.driveForwardByTime(-robot.DRIVE_SPEED, 500);
         robot.spin(true /* clockwise */, 2000 /* time */);
-        robot.moveArmByTime(0.5, 1500);
         robot.driveForwardByTime(robot.DRIVE_SPEED * 2, 1500);
-        robot.turnRightByTime(robot.DRIVE_SPEED, 2500);
-        robot.driveForwardByTime(robot.DRIVE_SPEED, 1000);
-        robot.moveArmByTime(-0.3,   250);
+        robot.turnRightByTime(robot.DRIVE_SPEED, 2250);
+        robot.moveArmByTime(0.75, 2000);
+        robot.driveForwardByTime(robot.DRIVE_SPEED * 2, 1300);
+        robot.moveArmByTime(-0.3,   350);
+        sleep(100);
         robot.claw.setPosition(0.0);
         robot.moveArmByTime(0.5, 350);
-        robot.driveForwardByTime(-robot.DRIVE_SPEED, 3000);
-        robot.strafeRightByTime(robot.DRIVE_SPEED, 1000);
+        robot.driveForwardByTime(-robot.DRIVE_SPEED * 2, 1300);
+        robot.strafeRightByTime(robot.DRIVE_SPEED, 1500);
     }
 }
 
